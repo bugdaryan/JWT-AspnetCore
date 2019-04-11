d4fc748b49f534995c45967aafea17b46cd0b685
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-11 18:42:45 +0400

    Add db context

---
+++ JWTAuth/JWTAuthApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                new {Id = "1", Name = "Admin", NormalizedData = "ADMIN"}, 
+                new {Id = "2", Name = "Costumer", NormalizedData = "CUSTOMER"}
+                );
+        }
+    }
+}
